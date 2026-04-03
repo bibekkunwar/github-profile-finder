18d085e8f4b80aa562c776cdfc69f463499ce659
bibekkunwar <bibek.kunwar@nitv.com.np>
2026-04-03 20:47:58 +1100

    added keydown when pressed enter to submit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,16 @@
 function SearchBar({ username, onChange, onSearch }) {
+
+  // Handle the "Enter" key press to trigger the search
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      onSearch();
+    }
+  }
   return (
     <div className="flex gap-2">
       <input
         onChange={onChange}
+        onKeyDown={handleKeyDown}
         placeholder="Enter a GitHub username"
         value={username}
         className="flex-1 px-4 py-2 rounded-lg border border-[#30363d] bg-[#161b22] text-sm text-[#e6edf3] placeholder:text-[#7d8590] outline-none focus:ring-2 focus:ring-[#238636]"
